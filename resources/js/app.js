641d827c3b8c19f5f9dbfefc9d5b4a42e5bc1d48
mohsinskipify <mohsinali@skipify.com>
2021-02-08 20:30:49 +0500

    Task Improvements

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import axios from 'axios';
-import GoogleSignInButton from 'vue-google-signin-button-directive';
 import Router  from 'vue-router';
 import Dashboard from './components/Dashboard';
 import Signup from "./components/Signup";
